1aa7fddba50ecfc1b80cbdd7880e940389f5f2da
BorisBritv <malcoach@mail.ru>
2021-11-03 15:25:48 +0300

    add Example005_If_Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower()=="boria")
+{
+    Console.WriteLine("ура,это же Boria!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
